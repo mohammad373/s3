90b164f8cd00ee24d5a9fc56bc7b9a7780e265b6
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2020-12-07 15:17:56 +0330

    Update and rename README.md to s3.py

---
+++ s3.py
@@ -0,0 +1,43 @@
+# s3
+# /wp-contenr/plugins/
+# 
+import json
+import os
+import requests
+import time
+import sys
+from colorama import Fore
+
+def __1__():
+    os.system("clear")
+    print(Fore.YELLOW + "Hello . Welcome Back ;) ")
+    target = input(Fore.BLUE + "\nEnter Your Address Target ==>  ")
+    if target == "" or None :
+        try:
+            time.sleep(2)
+            os.system("clear")
+            print(Fore.RED + "Ok Good Lunch ;)")
+        except:
+            pass
+    r = requests.get("http://" + target  "/wp-content/plugins/")
+    if r.status_code == 404 or r.status_code == 500:
+        try:
+            print(Fore.YELLOW + "\n[!] - Your Target Is Not WordPress ;)")
+            time.sleep(2)
+            sys.exit()
+        except:
+            pass
+    else:
+        try:
+            print(Fore.GREEN + "\n[+] - Your Target Is WordPress ;)")
+            time.sleep(2)
+            print(Fore.YELLOW + "\n[!] - This Is Checking The Your Target ...")
+            time.sleep(2)
+            print(Fore.YELLOW + "\n[!] - Pleass 3 sec Latter ...")
+            time.sleep(3)
+        except:
+            pass
+    j = requests.get("http://" + target + "/wp-json-wp/users/")
+    j1 = json.loads(j).text
+    print(j1)
+__1__()
